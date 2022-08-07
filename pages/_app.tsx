f12775eb2c22272e2f1b6f1548f075eb0ad92277
SolomonYakubu <soloyaks.sy@gmail.com>
2022-08-08 00:22:42 +0100

    updated _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,21 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <title>Athenasoft Technology</title>
         <style>
           @import
           url(&quot;https://fonts.googleapis.com/css2?family=Orbitron&family=Poppins&display=swap)
         </style>
+        <meta property="og:title" content="Athenasoft Technology" />
+
+        <meta name="author" content="Solomon Yakubu" />
+        <meta
+          property="og:description"
+          content="Athenasoft Technology help African businesses in developing and launching online, mobile application, and software projects more quickly and affordably.
+
+"
+        />
+        <meta property="og:url" content="https://athenasoftechnology.com.ng/" />
       </Head>
       <Component {...pageProps} />
     </>
